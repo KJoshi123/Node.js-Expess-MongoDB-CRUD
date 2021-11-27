ef9223677c756cd607cd540e950ef7b09b8f406c
kaustubh joshi <joshigikaustubh@gmail.com>
2021-11-27 13:09:35 +0530

    changed project directory

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config({path: __dirname + '../.env'});
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
